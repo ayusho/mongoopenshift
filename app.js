f0737a8d9921704bf2630f1976d593bfce3b454c
OJHA Ayush <ayush.ojha@soprasteria.com>
2018-02-13 17:36:16 +0530

    first commit mongo node

---
+++ app.js
@@ -0,0 +1,35 @@
+var express = require("express");
+var app = express();
+var port = 3000;
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DATABASE_SERVICE_NAME+"://"+process.env.MONGODB_USER+":"+ process.env.MONGODB_PASSWORD+"@localhost" +":27017/"+process.env.MONGODB_DATABASE);
+//mongoose.connect("mongodb://localhost:27017/node-demo");
+var nameSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String
+});
+var User = mongoose.model("User", nameSchema);
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/addname", (req, res) => {
+    var myData = new User(req.body);
+    myData.save()
+        .then(item => {
+            res.send("Name saved to database");
+        })
+        .catch(err => {
+            res.status(400).send("Unable to save to database");
+        });
+});
+
+app.listen(port, () => {
+    console.log("Server listening on port " + port);
+});
